cd5b485d2e5497046c8db09fe8c866fbafa3b62d
james-hardy <topsec115@gmail.com>
2021-04-29 14:27:09 -0400

    changed image path to match image that is rendering

--- src/components/Project.js
+++ src/components/Project.js
@@ -59,7 +59,7 @@ const cards = [
 
   {
     key: 2,
-    image: "./assets/graph.png",
+    image: "assets/graph.png",
     title: "Workout Tracker",
     description:
       "Stay on track with your fitness goals and health by tracking your workouts, your progress, and add new workouts to your routine",
@@ -70,7 +70,7 @@ const cards = [
 
   {
     key: 3,
-    image: "./assets/looking4balance.png",
+    image: "assets/looking4balance.png",
     title: "Looking 4 Balance",
     description:
       "Balance! With such a busy life, we all need balance to assure we never forget the important things in life. That is why this app allows you to select the 3 most important things you need to accomplish TODAY! 3 things to do with your family, work, and school. Start the day with an inspiring quote, and come back to your site when you need a laugh.",
@@ -80,7 +80,7 @@ const cards = [
 
   {
     key: 4,
-    image: "./assets/scheduler-screen-shot.png",
+    image: "assets/scheduler-screen-shot.png",
     title: "Work day scheduler",
     description: 
       "Stay organized by planning your day hour by hour and see at a glance where you are in the day",
@@ -90,7 +90,7 @@ const cards = [
 
   {
     key: 5,
-    image: "./assets/travel-logger.png",
+    image: "assets/travel-logger.png",
     title: "Travel Logger",
     description: 
       "Do you like to travel? So do we, and so do all of your friends. Let's keep track of the places we travel, and blog about what we like at each place we visit for all of us to see and add our own comments. We may want to go visit the same places. Not sure where to go? Get in your car and drive, and use this app to find places to do based on your interest",
@@ -100,7 +100,7 @@ const cards = [
 
   {
     key: 6,
-    image: "./assets/weatherapp.png",
+    image: "assets/weatherapp.png",
     title: "Weather App",
     description: "Do have a trip planned? Wouldn't it be great to keep all of the places you will visit in one place, to quickly see and plan according to the weather? This is your app! See the weather for your selected cities, along with a 5 day forcast, and even a quick view of the current weather with a matching gif at the top of the page",
     gitHubRepo: "https://github.com/HectorDiazJr/Weather-App",
