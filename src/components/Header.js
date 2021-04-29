50c270f57c004717a617917a1a6ff7d96e4bcc84
james-hardy <topsec115@gmail.com>
2021-04-29 14:06:46 -0400

    trying to render images

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import AppBar from '@material-ui/core/AppBar';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+
 
 
 export default function Album() {
@@ -24,17 +24,3 @@ export default function Album() {
   );
 }
 
-{/* <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-  Open Menu
-</Button>
-<Menu
-  id="simple-menu"
-  anchorEl={anchorEl}
-  keepMounted
-  open={Boolean(anchorEl)}
-  onClose={handleClose}
->
-  <MenuItem onClick={handleClose}>Profile</MenuItem>
-  <MenuItem onClick={handleClose}>My account</MenuItem>
-  <MenuItem onClick={handleClose}>Logout</MenuItem>
-</Menu> */}
--- src/components/Project.js
+++ src/components/Project.js
@@ -12,7 +12,7 @@ import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-import Cards from "./Cards";
+// import Cards from "./Cards";
 // import Link from '@material-ui/core/Link';
 
 
@@ -49,7 +49,7 @@ const cards = [
   //images are in public/assests = path is simply assets/filename
   {
     key: 1,
-    image: "/assets/burger.png",
+    image: "assets/burger.png",
     title: "Burger App",
     description:
       "So many burgers, so little time! Add all of the burgers you would like to try, and keep track of all the burgers you have devoured.",
@@ -59,7 +59,7 @@ const cards = [
 
   {
     key: 2,
-    image: "/assets/graph.png",
+    image: "assets/graph.png",
     title: "Workout Tracker",
     description:
       "Stay on track with your fitness goals and health by tracking your workouts, your progress, and add new workouts to your routine",
@@ -70,7 +70,7 @@ const cards = [
 
   {
     key: 3,
-    image: "/assets/Looking4Balance.png",
+    image: "assets/Looking4Balance.png",
     title: "Looking 4 Balance",
     description:
       "Balance! With such a busy life, we all need balance to assure we never forget the important things in life. That is why this app allows you to select the 3 most important things you need to accomplish TODAY! 3 things to do with your family, work, and school. Start the day with an inspiring quote, and come back to your site when you need a laugh.",
@@ -80,7 +80,7 @@ const cards = [
 
   {
     key: 4,
-    image: "/assets/Scheduler-screen-shot.png",
+    image: "assets/Scheduler-screen-shot.png",
     title: "Work day scheduler",
     description: 
       "Stay organized by planning your day hour by hour and see at a glance where you are in the day",
@@ -100,7 +100,7 @@ const cards = [
 
   {
     key: 6,
-    image: "/assets/WeatherApp.png",
+    image: "assets/WeatherApp.png",
     title: "Weather App",
     description: "Do have a trip planned? Wouldn't it be great to keep all of the places you will visit in one place, to quickly see and plan according to the weather? This is your app! See the weather for your selected cities, along with a 5 day forcast, and even a quick view of the current weather with a matching gif at the top of the page",
     gitHubRepo: "https://github.com/HectorDiazJr/Weather-App",
